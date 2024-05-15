4b37e5b10f83ee5d9ca4fbf30ddc17df067e7975
song <tedsong3170@gmail.com>
2024-05-15 18:40:25 +0900

    에러 코드 정리
    - PM : Payment Method
    - PT : Payment Token
    - CM : Common
    - PS : Payment SyStem
    
    - S : 서버
    - C : 클라이언트

--- src/main/java/song/pg/payment/utils/ExceptionEnum.java
+++ src/main/java/song/pg/payment/utils/ExceptionEnum.java
@@ -5,23 +5,23 @@ import lombok.Getter;
 @Getter
 public enum ExceptionEnum
 {
-  ALREADY_EXIST_PAYMENT(400, "1001", "이미 결제가 진행중인 주문입니다."),
-  UNKNOWN_PAYMENT_METHOD(500, "1002", "알 수 없는 결제 수단입니다."),
-  INVALID_TOKEN(401, "1003", "유효하지 않은 토큰입니다."),
-  ENCRYPT_ERROR(500, "1004", "암호화 중 오류 발생하였습니다."),
-  DECRYPT_ERROR(500, "1005", "복호화 중 오류 발생하였습니다."),
+  UNKNOWN_PAYMENT_METHOD(500, "SPM0001", "알 수 없는 결제 수단입니다."),
+  ALREADY_EXIST_NICKNAME(400, "CPM0001", "이미 등록된 별칭입니다."),
+  INVALID_CARD_NUMBER(400, "CPM0002", "유효하지 않은 카드 번호입니다."),
 
-  ALREADY_EXIST_NICKNAME(400, "1006", "이미 등록된 별칭입니다."),
-  INVALID_CARD_NUMBER(400, "1007", "유효하지 않은 카드 번호입니다."),
-  INVALID_JSON_FORMAT(400, "1008", "유효하지 않은 JSON 형식입니다."),
-  INVALID_TO_JSON_FORMAT(400, "1009", "유효하지 않은 JSON 직렬화 형식입니다."),
+  INVALID_TOKEN(401, "CAU0001", "유효하지 않은 토큰입니다."),
 
-  NOT_EXIST_PAYMENT(400, "1010", "존재하지 않는 결제입니다."),
+  ENCRYPT_ERROR(400, "CCM0001", "암호화 중 오류 발생하였습니다."),
+  DECRYPT_ERROR(400, "CCM0002", "복호화 중 오류 발생하였습니다."),
+  INVALID_JSON_FORMAT(400, "CCM0003", "유효하지 않은 JSON 형식입니다."),
+  INVALID_TO_JSON_FORMAT(400, "CCM0004", "유효하지 않은 JSON 직렬화 형식입니다."),
 
-  INVALID_PAYMENT_REQUEST(400, "1011", "유효하지 않은 결제 요청입니다."),
+  ALREADY_EXIST_PAYMENT(400, "CPS0001", "이미 결제가 진행중인 주문입니다."),
+  NOT_EXIST_PAYMENT(400, "CPS0002", "존재하지 않는 결제입니다."),
+  INVALID_PAYMENT_REQUEST(400, "CPS0003", "유효하지 않은 결제 요청입니다."),
+  NOT_EXIST_PAYMENT_LEDGER(400, "CPS0004", "존재하지 않는 결제원장입니다."),
 
-  NOT_EXIST_PAYMENT_LEDGER(400, "1012", "존재하지 않는 결제원장입니다."),
-
+  UNKNOWN_ERROR(500, "CCM9999", "알 수 없는 오류가 발생하였습니다."),
   ;
 
   private final int status;
